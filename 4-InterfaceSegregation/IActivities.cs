89aeec22da1cbcfbe5fe8de12294a9d1c8b89489
Corozco <orozcomondragons@gmail.com>
2024-01-30 15:28:18 -0500

    Se hace un cambio con las funciones

--- 4-InterfaceSegregation/IActivities.cs
+++ 4-InterfaceSegregation/IActivities.cs
@@ -1,11 +1,9 @@
 namespace InterfaceSegregation
 {
-    public interface IActivities
+    //Si se desea que una sola interfaz implemente todos los metodos de todas las demás interfaces.
+    //Se puede hacer herencia entre interfaces y asi una sola interfaz podrá implementar todos los metodos
+    public interface IActivities : IComunicate, IDesign, IDevelop, ITest, IPlan
     {
-        void Plan();
-        void Comunicate();
-        void Design();
-        void Develop();
-        void Test();
+        
     }
 }
