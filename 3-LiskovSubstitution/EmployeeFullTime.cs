1a85db4dd103ccf0ea5f9f4946d3f17b9a67d50b
Corozco <orozcomondragons@gmail.com>
2024-01-30 14:27:53 -0500

    Se realiza el calculo de su salario dentro de la misma clase, tomando en cuenta las horas extra

--- 3-LiskovSubstitution/EmployeeFullTime.cs
+++ 3-LiskovSubstitution/EmployeeFullTime.cs
@@ -2,8 +2,20 @@ namespace Liskov
 {
     public class EmployeeFullTime : Employee
     {
-        public EmployeeFullTime(string fullname, int hoursWorked, int extrahours) : base(fullname, hoursWorked, extrahours)
-        {
+        public int ExtraHours{get;set;}
+
+        //Definicion del constructor
+        public EmployeeFullTime(string fullname, int hoursWorked, int extrahours) : base(  fullname,  hoursWorked){
+            //Solo es necesaria realizar la inicialización de la variable de horas extra para este empleado
+            ExtraHours=extrahours;
         }
+
+        //Formula para calcular el salario
+        public override decimal CalculateSalary()
+        {
+            decimal valorHora = 50M;
+            return valorHora * (HoursWorked + ExtraHours);
+        }
+
     }
 }
