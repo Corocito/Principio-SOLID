02b1768cd919667579715b165f56118fe966624d
Corozco <orozcomondragons@gmail.com>
2024-01-30 14:27:03 -0500

    Se realiza el calculo de su salario dentro de la misma clase, sin tomar en cuenta las horas extra

--- 3-LiskovSubstitution/EmployeeContractor.cs
+++ 3-LiskovSubstitution/EmployeeContractor.cs
@@ -2,8 +2,15 @@ namespace Liskov
 {
     public class EmployeeContractor : Employee
     {
-        public EmployeeContractor(string fullname, int hoursWorked, int extrahours) : base(fullname, hoursWorked, extrahours)
+
+        //Definicion del constructor    
+        public EmployeeContractor(string fullname, int hoursWorked) : base(fullname, hoursWorked){
+        }
+
+        public override decimal CalculateSalary()
         {
+            decimal valorHora = 40M;
+            return valorHora * HoursWorked;
         }
     }
-}
+} 
